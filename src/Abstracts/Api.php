7a444f6e2cfd7569e58fd05256f992304d215005
Bruno Škvorc <bruno@skvorc.me>
2015-01-22 11:29:10 +0000

    Updated for Part 3 start

--- src/Abstracts/Api.php
+++ src/Abstracts/Api.php
@@ -11,6 +11,31 @@ abstract class Api
     /** @var int Timeout value in ms - defaults to 30s if empty */
     private $timeout = 30000;
 
+    /** @var string The URL onto which to unleash the API in question */
+    private $url;
+
+    public function __construct($url)
+    {
+        if (!is_string($url)) {
+            throw new \InvalidArgumentException('URL param must be a string.');
+        }
+        $url = trim($url);
+        if (strlen($url) < 4) {
+            throw new \InvalidArgumentException('URL must be at least four characters in length');
+        }
+        if ($parts = parse_url($url)) {
+            if (!isset($parts["scheme"])) {
+                $url = "http://$url";
+            }
+        }
+        $filtered_url = filter_var($url, FILTER_VALIDATE_URL);
+        if (false === $filtered_url) {
+            throw new \InvalidArgumentException('You provided an invalid URL: ' . $url);
+        }
+
+        $this->url = $filtered_url;
+    }
+
     /**
      * Setting the timeout will define how long Diffbot will keep trying
      * to fetch the API results. A timeout can happen for various reasons, from
--- tests/Abstracts/ApiTest.php
+++ tests/Abstracts/ApiTest.php
@@ -6,12 +6,15 @@ use Swader\Diffbot\Abstracts\Api;
 
 class ApiTest extends \PHPUnit_Framework_TestCase
 {
+    private $className = 'Swader\Diffbot\Abstracts\Api';
+    private $testUrl = 'http://diffbot.com';
+
     /**
      * @return \PHPUnit_Framework_MockObject_MockObject
      */
     private function buildMock()
     {
-        return $this->getMockForAbstractClass('Swader\Diffbot\Abstracts\Api');
+        return $this->getMockForAbstractClass($this->className, [$this->testUrl]);
     }
 
     public function testSetTimeout()
@@ -60,5 +63,57 @@ class ApiTest extends \PHPUnit_Framework_TestCase
             }
             $this->fail('Failed, assumed invalid parameter was valid.');
         }
+    }
+
+    public function testConstructor()
+    {
+        $validUrls = [
+            'http://google.com',
+            'http://gigaom.com/cloud/silicon-valley-royalty-pony-up-2m-to-scale-diffbots-visual-learning-robot/',
+            <<<'TAG'
+http://techcrunch.com/2012/05/31/diffbot-raises-2-million-seed-round-for-web-content-extraction-technology/
+TAG
+            ,
+            'http://www.theverge.com/2012/5/31/3054444/diffbot-raises-2-million-apps-open-web',
+            'http://venturebeat.com/2012/08/16/diffbot-api-links',
+            'http://www.wired.co.uk/news/archive/2012-06/01/diffbot',
+            'http://www.amazon.com/Oh-The-Places-Youll-Go/dp/0679805273/',
+            'http://us.levi.com/product/index.jsp?productId=2076855',
+            <<<'TAG'
+http://www.petsmart.com/dog/grooming-supplies/grreat-choice-soft-slicker-dog-brush-zid36-12094/cat-36-catid-100016
+TAG
+            ,
+            'http://instagram.com/p/t879OvgvqS/',
+            'http://smittenkitchen.com/blog/2012/01/buckwheat-baby-with-salted-caramel-syrup/',
+            'https://twitter.com/NASA/status/525397368116895744',
+            'www.example.com',
+            'example.com'
+        ];
+
+        $invalidUrls = [
+            false,
+            null,
+            12345,
+            'abc',
+            '35tugz---sdf----?//*****/*//*'
+        ];
+
+        foreach ($validUrls as $i => $url) {
+            try {
+                $this->getMockForAbstractClass($this->className, [$url]);
+            } catch (\InvalidArgumentException $e) {
+                $this->fail('Failed with supposedly valid URL: ' . $url . ' at index ' . $i);
+            }
+        }
+
+        foreach ($invalidUrls as $i => $url) {
+            try {
+                $this->getMockForAbstractClass($this->className, [$url]);
+            } catch (\InvalidArgumentException $e) {
+                continue;
+            }
+            $this->fail('Did not fail with invalid URL at index ' . $i);
+        }
+
     }
 }
